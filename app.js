d9155e3ad109769a11cfc6b4c8347e7a3b8e6672
Marcelo Mendes <>
2021-09-03 00:52:16 -0300

    Arquivo responsável por logar no sistema e baixar o relatório em pdf

---
+++ app.js
@@ -0,0 +1,75 @@
+const express = require('express');
+const app = express();
+
+const http = require("http");
+const puppeteer = require('puppeteer'); 
+const delay = require('delay');
+const path = require('path');
+const mkdirp = require('mkdirp');
+var fs = require('fs');
+var os = require('os');
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Dete Soluções - www.detesolucoes.com.br");
+});
+
+/**
+ * @param {page from where you want to download file } page
+ * @param {downloadLocation , directory where you want to save a file } downloadLocation
+ */
+async function downloadFile(page, downloadLocation){
+    const downloadPath = path.resolve(downloadLocation)
+    mkdirp(downloadPath)
+    await page._client.send('Page.setDownloadBehavior', {
+        behavior: 'allow',
+        downloadPath: downloadPath,
+    })
+}
+
+app.post("/get-report", (req, res) => {
+    (async () => {    
+        const browser = await puppeteer.launch({
+            executablePath: '/usr/bin/chromium-browser',
+            headless: true,
+        });
+
+        const page = await browser.newPage();
+
+        await page.goto(req.body.urlLogin);
+        await delay(1000);
+        await page.goto(req.body.urlReport);
+
+        await downloadFile(page, req.body.directoryFiles);
+
+        await page.type('[name="User"]', req.body.access_user);
+        await page.type('[name="Password"]', req.body.access_pass);
+        await page.click('[value="View Report"]');
+
+        await delay(3000);
+
+        // renomeando o arquivo
+        fs.rename(__dirname+'/'+req.body.directoryFiles+'/reportviewer.pdf', __dirname+'/'+req.body.directoryFiles+'/'+req.body.file, function(err) {
+            if ( err ){ 
+                console.log('ERROR: ' + err);
+                return;
+            }
+        });
+
+        await browser.close();
+
+        try {
+            if (fs.existsSync(pathFile)) {
+                return res.json({status: "success", message: "Relatório gerado com sucesso", file: pathFile, error: null});
+            }
+        } catch(err) {
+            console.error("Ops! "+err)
+            return res.json({status: "error", message: "Erro ao gerar relatório", file: null, error: err});
+        }
+    })();
+});
+
+app.listen(3333, () => {
+    console.log("Servidor iniciado.");
+});
